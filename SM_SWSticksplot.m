8bc71d52954694815007f342e03bbb25dd5c862a
Alan Baird <alan.baird@bristol.ac.uk>
2012-02-24 14:16:11 +0000

    no message

--- SM_SWSticksplot.m
+++ SM_SWSticksplot.m
@@ -1,2 +1,3 @@
+
 test=SM_import_ec_cell(fname);
-fign=SM_SWSticks(test(2:end-1,2:end-1,17),[savename '.SWSticks.eps']);
+fign=SM_SWSticks(test(2:end-1,2:end-1,zlevel),[savename num2str(test(1,1,zlevel).z) '.SWSticks.eps']);
